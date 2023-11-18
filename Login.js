348871ce0da347f616a2169d720fb645b27b17e7
saabsinghh <140698164+saabsinghh@users.noreply.github.com>
2023-11-18 10:36:16 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import {mobile} from "../components/responsive"
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://images.unsplash.com/photo-1529220355416-122440146f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fGNsb3RoZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60")
+      center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  width: 26%;
+  
+  padding: 20px;
+  background-color: white;
+  ${mobile({ width: "75%" })}
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Link = styled.a`
+  margin: 5px 0px;
+  font-size: 12px;
+  text-decoration: underline;
+  cursor: pointer;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>DO NOT YOU REMEMBER THE PASSWORD?</Link>
+          <Link>CREATE A NEW ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import axios from "axios";
+//import { useState } from "react";
+//<link rel="stylesheet" href="./App.css"></link>
+import  '../App.css'
+//import Categories from './src/components/Categories';
+import Categories from '../components/Categories';
+//import Footer from '../components/Footer';
+import Footer from '../components/Footer';
+//import Navbar from './src/components/Navbar';
+//import Slider from './components/slider';
+import Navbar from '../components/Navbar';
+ 
+const Home  = () =>  {
+  return (
+    
+    <><div className='navbar'>
+    <Navbar/>
+    </div>
+    <img src='https://images.unsplash.com/photo-1603798125914-7b5d27789248?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60' alt='clothes' className='img' />
+    <div className='flex1'>
+      <img src='https://images.unsplash.com/photo-1516762689617-e1cffcef479d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGNsb3RoZXMlMjBob3Jpem9udGFsfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' alt='clothes' className='img2'/>
+      <div style={{display:'flex', flexDirection:'column'}}>
+      <div className='estore'>E-Store</div>
+      <div className='welcome'><p>HI,Welcome to E-Store .Here you will find a huge range of products also of many aspects like shirts,jeans,handbags.Also we have very reasonable prices cant will not disappoint you .So,you are waiting for What?Just start the shopping!!!!</p></div>
+      </div>
+    </div>
+    <Categories/>
+    <Footer/>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+         </>  
+ )
+};
+export default Home;
+  
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
